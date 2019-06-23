722adca7d46cf96beecf2d822578d23ddb1fd579
spencerchurchill <spencer.churchill@outlook.com>
2019-06-23 12:21:46 -0700

    initial commit for quantum rng
    uses https://qrng.anu.edu.au/ for quantum random numbers

---
+++ send.sh
@@ -0,0 +1,10 @@
+#echo "Input specific change: "
+#read change
+#git config --global user.email "spencer.churchill@outlook.com"
+#git config --global user.name "spencerchurchill"
+#git add --all
+#git pull
+#git commit -m "$change"
+#git push -u origin master
+
+zip -r -9 --exclude=*.git* --exclude=*.DS_Store* --exclude=*.vscode* --exclude=*.sh* ../q-rng.zip *
