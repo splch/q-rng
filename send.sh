eba7e610320842041462b539f0173f9757a5708f
spencerchurchill <spencer.churchill@outlook.com>
2019-06-24 12:28:10 -0700

    Update send.sh

--- send.sh
+++ send.sh
@@ -7,4 +7,5 @@
 #git commit -m "$change"
 #git push -u origin master
 
+rm .DS_Store
 zip -r -9 --exclude=*.git* --exclude=*.DS_Store* --exclude=*LICENSE* --exclude=*.vscode* --exclude=*.sh* ../q-rng.zip *
