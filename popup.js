45f109483dfc46de0ef58d3f3103f0b0013f3550
Spencer Churchill <spencer.churchill@outlook.com>
2019-06-23 21:20:55 -0700

    minor performance updates

--- popup.js
+++ popup.js
@@ -1,24 +1,19 @@
-document.getElementById("gen").onclick = function() {
+function setQRN() {
     document.getElementById("qrn").innerHTML = '';
     let settings = {
-        "async": true,
-        "crossDomain": true,
         "url": "https://qrng.anu.edu.au/API/jsonI.php",
         "method": "GET",
-        "headers": {
-            "content-type": "application/x-www-form-urlencoded",
-        },
         "data": {
             "length": "1",
             "type": "uint16",
         }
     };
-    $.ajax(settings).always(function (data) {
+    $.ajax(settings).done(function (data) {
         let min = parseInt(document.getElementById("min").value);
         let max = parseInt(document.getElementById("max").value);
-        let qrn = Math.round(data.data[0] / 65535 * (max - min) + min);
-        if (qrn) {
-            document.getElementById("qrn").innerHTML = qrn;
-        }
+        let qrn = Math.round(data.data[0] / 65535 * (max - min) + min); // scale the random number
+        document.getElementById("qrn").innerHTML = qrn;
     });
 }
+
+document.getElementById("gen").onclick = setQRN;
