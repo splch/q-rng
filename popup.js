416958c44226ee5d92b4c13c2855a21e62a2ad2d
spencerchurchill <spencer.churchill@outlook.com>
2019-06-23 23:57:45 -0700

    error checking

--- popup.js
+++ popup.js
@@ -1,19 +1,32 @@
 function setQRN() {
+    let min = parseInt(document.getElementById("min").value);
+    let max = parseInt(document.getElementById("max").value);
     document.getElementById("qrn").innerHTML = '';
-    let settings = {
-        "url": "https://qrng.anu.edu.au/API/jsonI.php",
-        "method": "GET",
-        "data": {
-            "length": "1",
-            "type": "uint16",
+    if (!min || !max) {
+        document.getElementById("qrn").innerHTML = 0;
+    }
+    else if (max - min > 65536) {
+        document.getElementById("qrn").innerHTML = "&#128565;";
+    }
+    else {
+        if (min > max) {
+            min = parseInt(document.getElementById("max").value);
+            max = parseInt(document.getElementById("min").value);
+            document.getElementById("min").value = min;
+            document.getElementById("max").value = max;
         }
-    };
-    $.ajax(settings).done(function (data) {
-        let min = parseInt(document.getElementById("min").value);
-        let max = parseInt(document.getElementById("max").value);
-        let qrn = Math.round(data.data[0] / 65535 * (max - min) + min); // scale the random number
-        document.getElementById("qrn").innerHTML = qrn;
-    });
+        const settings = {
+            "url": "https://qrng.anu.edu.au/API/jsonI.php",
+            "method": "GET",
+            "data": {
+                "length": "1",
+                "type": "uint16",
+            }
+        };
+        $.ajax(settings).done(function (data) {
+            document.getElementById("qrn").innerHTML = Math.round(data.data[0] / 65535 * (max - min) + min); // scale the random number;
+        });
+    }
 }
 
 document.getElementById("gen").onclick = setQRN;
