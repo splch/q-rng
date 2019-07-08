5d7752de064f1c54a4096bd50ed97f0daaad349f
spencerchurchill <spencer.churchill@outlook.com>
2019-07-08 14:30:16 -0700

    method update
    crypto -> window.crypto

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 function _prng(vals) {
-    let prn = Math.floor(crypto.getRandomValues(new Uint32Array(1))[0] / 4294967296 * 8177608);
+    let prn = Math.floor(window.crypto.getRandomValues(new Uint32Array(1))[0] / 4294967296 * 8177608);
     if (qrn) {
         document.getElementById("rn").title = "QRNG";
         load.index++;
@@ -7,7 +7,7 @@ function _prng(vals) {
     }
     else {
         document.getElementById("rn").title = "PRNG";
-        prn = prn * crypto.getRandomValues(new Uint16Array(1))[0];
+        prn = prn * window.crypto.getRandomValues(new Uint16Array(1))[0];
     }
     if (prn === 0) {
         prn += 2147483646;
