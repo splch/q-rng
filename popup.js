d27b7e9c1d4bf54e62a61ef18d4df1d173d6db98
spencerchurchill <spencer.churchill@outlook.com>
2019-07-19 10:54:09 -0700

    removed repeat cursor style function

--- popup.js
+++ popup.js
@@ -11,14 +11,14 @@ function load(bounds, len, web) {
             document.getElementById("rn").style.color = "#666666";
         }
         document.getElementById("rn").innerText = prng(bounds, len);
-        document.body.style.cursor = "auto";
-        return;
     }
-    if (rn.win) rn.win.close();
-    let qrns = [];
-    for (let i = 0; i < len; i++) qrns.push(prng(bounds, len));
-    rn.win = window.open("", "_blank", "width=175,height=128", true);
-    rn.win.document.write("<a href=data:text/plain,"+qrns+" download='qrns-"+bounds+"'>Save</a>" + (web ? "<p style='word-break: break-all; color: #222222;'>"+qrns+"</p>" : "<p style='word-break: break-all; color: #666666;' title='Error: PRNG'>"+qrns+"</p>"));
+    else {
+        if (rn.win) rn.win.close();
+        let qrns = [];
+        for (let i = 0; i < len; i++) qrns.push(prng(bounds, len));
+        rn.win = window.open("", "_blank", "width=175,height=128", true);
+        rn.win.document.write("<a href=data:text/plain,"+qrns+" download='qrns-"+bounds+"'>Save</a>" + (web ? "<p style='word-break: break-all; color: #222222;'>"+qrns+"</p>" : "<p style='word-break: break-all; color: #666666;' title='Error: PRNG'>"+qrns+"</p>"));
+    }
     document.body.style.cursor = "auto";
 };
 
